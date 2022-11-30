edb978025f79c2513bfe7c8c8db930eddce455da
tannerGL <tanner_gerard@proton.me>
2022-11-29 23:59:45 -0300

    first board display

--- App/app.py
+++ App/app.py
@@ -30,6 +30,10 @@ def index():
     conn.close()
     return render_template('index.html', posts=posts)
 
+@app.route('/game')
+def game():
+    return render_template('game.html')
+
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
