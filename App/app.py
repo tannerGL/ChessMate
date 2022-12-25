adc1a6cf94a671c8e8bb769088ac3b0fc24bbcf5
tannerGL <tanner_gerard@proton.me>
2022-12-25 14:20:34 -0500

    sending to server from javascript

--- App/app.py
+++ App/app.py
@@ -11,27 +11,17 @@ from stockfish.stockfish import Stockfish
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'tbuhquw98kd7hda0kdk2hbtrq2j4k3l2p1h5th9q'
 
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
-
-def get_post(post_id):
-    conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
-    conn.close()
-    if post is None:
-        abort(404)
-    return post
-
 @app.route('/')
 def index():
-    return render_template('index.html', posts=posts)
+    return render_template('index.html')
 
 @app.route('/create_account')
 def create_account():
     return render_template('create_account.html')
+
+@app.route('/attempt_create', methods = ['POST'])
+def attempt_create(): # attempt to create an account
+    return render_template('game.html')
 
 @app.route('/login')
 def login():
@@ -43,7 +33,6 @@ def login():
         return render_template('index.html')
 
     return render_template('login.html')
-
 
 
 @app.route('/game')
@@ -63,51 +52,51 @@ def stockfish_query_move(current_fen, move):
     
     return (fen, score_or_bad_move)
 
-@app.route('/create', methods=('GET', 'POST'))
-def create():
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
+# @app.route('/create', methods=('GET', 'POST'))
+# def create():
+#     if request.method == 'POST':
+#         title = request.form['title']
+#         content = request.form['content']
 
-        if not title:
-            flash('Title is required!')
-        else:
-            conn = get_db_connection()
-            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
-            conn.commit()
-            conn.close()
-            return redirect(url_for('index'))
+#         if not title:
+#             flash('Title is required!')
+#         else:
+#             conn = get_db_connection()
+#             conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+#                          (title, content))
+#             conn.commit()
+#             conn.close()
+#             return redirect(url_for('index'))
 
-    return render_template('create.html')
+#     return render_template('create.html')
 
-@app.route('/<int:id>/edit', methods=('GET', 'POST'))
-def edit(id):
-    post = get_post(id)
+# @app.route('/<int:id>/edit', methods=('GET', 'POST'))
+# def edit(id):
+#     post = get_post(id)
 
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
+#     if request.method == 'POST':
+#         title = request.form['title']
+#         content = request.form['content']
 
-        if not title:
-            flash('Title is required!')
-        else:
-            conn = get_db_connection()
-            conn.execute('UPDATE posts SET title = ?, content = ?'
-                         ' WHERE id = ?',
-                         (title, content, id))
-            conn.commit()
-            conn.close()
-            return redirect(url_for('index'))
+#         if not title:
+#             flash('Title is required!')
+#         else:
+#             conn = get_db_connection()
+#             conn.execute('UPDATE posts SET title = ?, content = ?'
+#                          ' WHERE id = ?',
+#                          (title, content, id))
+#             conn.commit()
+#             conn.close()
+#             return redirect(url_for('index'))
 
-    return render_template('edit.html', post=post)
+#     return render_template('edit.html', post=post)
 
-@app.route('/<int:id>/delete', methods=('POST',))
-def delete(id):
-    post = get_post(id)
-    conn = get_db_connection()
-    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
-    conn.commit()
-    conn.close()
-    flash('"{}" was successfully deleted!'.format(post['title']))
-    return redirect(url_for('index'))
+# @app.route('/<int:id>/delete', methods=('POST',))
+# def delete(id):
+#     post = get_post(id)
+#     conn = get_db_connection()
+#     conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+#     conn.commit()
+#     conn.close()
+#     flash('"{}" was successfully deleted!'.format(post['title']))
+#     return redirect(url_for('index'))
--- App/static/scripts/login.js
+++ App/static/scripts/login.js
@@ -2,15 +2,38 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
+
+function authenticate(user, pass)
+{
+    var success = false;
+    $.ajax({
+        type: "POST",
+        url: '/attempt_create', 
+        data: {
+            username: user,
+            password: pass,
+        },
+    success: function(data) {
+        console.log(data);
+        success = true;
+        alert("SUCCESS");
+    },
+    });
+    return success;
+}
+
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "user" && password === "web_dev") {
-        alert("You have successfully logged in.");
-        location.reload();
-    } else {
+    if (authenticate(username,password) === true){
+        $.ajax({
+            type: "GET",
+            url: '/game',
+        });
+    }
+    else {
         loginErrorMsg.style.opacity = 1;
     }
-})
+});
